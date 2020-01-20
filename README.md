4ed751bb3a861923e077660855abe43416ddc20e
Andre Cianflone <apcianflone@gmail.com>
2020-01-20 17:31:43 -0500

    add run script

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # non-mon
 [under construction]
 
+The baseline is ordered-neuron LSTM, which proposes a new mechanism for the input and forget gates, see [this slide](https://youtu.be/7REBftHDQOw?t=398)
 To run the baseline without the mechanism:
 ```
 ./ptb_lm.sh
 ```
 
-The baseline RNN code is based on ordered-neuron by Shen et al, see [paper](https://arxiv.org/abs/1810.09536) and [code](https://github.com/yikangshen/Ordered-Neurons)
+For more on ordered-neuron by Shen et al 2018, see [paper](https://arxiv.org/abs/1810.09536) and [code](https://github.com/yikangshen/Ordered-Neurons)
 
---
+++ run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+CLEAR='\033[0m'
+RED='\033[0;31m'
+
+function usage() {
+  if [ -n "$1" ]; then
+    echo -e "${RED} $1${CLEAR}\n";
+  fi
+  exit 1
+}
+
+# Debug or not
+BASE='python main.py'
+if [ -n "$2" ]; then
+    if [[ $2 == *"debug"* ]]; then
+        BASE='python -m pudb main.py'
+    fi
+fi
+
+case "$1" in
+        1)
+            echo "Baseline"
+            eval $BASE \
+                --batch_size 20
+                --dropout 0.45
+                --dropouth 0.3
+                --dropouti 0.5
+                --wdrop 0.45
+                --chunk_size 10
+                --seed 141
+                --epoch 1000
+            ;;
+
+        2)
+            echo "Non-mon"
+            eval $BASE \
+                --batch_size 20
+                --dropout 0.45
+                --dropouth 0.3
+                --dropouti 0.5
+                --wdrop 0.45
+                --chunk_size 10
+                --seed 141
+                --epoch 1000
+            ;;
+        *)
+            usage "You need to call $0 with an int option"
+            exit 1
+esac
+
